a95407cd53e91d80f79a5e6cdc776b1d76ac5ec5
Vagner Paulino <vagnerpaulinop@gmail.com>
2019-05-28 22:15:37 -0300

    Finalização da segurança do BackEnd

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,14 +1,29 @@
 const express = require('express')
+const auth = require ('./auth')
 
 module.exports = function(server) {
 
+    /*
+    * Rotas protegidas por Token JWT
+    */
 
-    // Definir URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
-    // Rotas de Ciclo de Pagamento
+    protectedApi.use(auth)
+
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
 
+    /*
+    * Rotas abertas
+    */
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validaToken)
 }
